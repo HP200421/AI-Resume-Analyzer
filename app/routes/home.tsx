92af6949b541631777dc7331642c52cfd938784e
Haridas Pawar <haridaspawar0203@gmail.com>
2025-08-12 23:33:27 +0530

    Navbar and Home Page Done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "AI Resume Analyzer for Potential Job Seekers" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Smart Tracking for Your Job Hunt & Resume Feedback</h1>
+        <h2>AI That Speaks HR's Language</h2>
+      </div>
+
+      {resumes.length > 0 &&
+        (<div className="resumes-section">
+          {resumes.map((resume) => (
+            <div>
+              <ResumeCard key={resume.id} resume={resume} />
+            </div>
+          ))}
+        </div>)
+      }
+    </section>
+
+  </main>
 }
