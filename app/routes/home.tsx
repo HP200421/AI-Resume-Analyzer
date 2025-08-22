904439b62e8ba120f7349701aa3035c875b2d9a0
Haridas Pawar <haridaspawar0203@gmail.com>
2025-08-22 07:32:26 +0530

    Implemet Wipe Functionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -48,7 +48,7 @@ export default function Home() {
       </div>
 
       {
-        loadingResume && (
+         loadingResume && (
           <div className="flex flex-col items-center justify-center">
             <img src="/images/resume-scan-2.gif" alt="Resume Loadinf" className="w-[200px]" />
           </div>
