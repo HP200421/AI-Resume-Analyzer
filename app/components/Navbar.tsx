f99e01d886f22a49c777805506d9193e3eaec290
Haridas Pawar <haridaspawar0203@gmail.com>
2025-08-24 12:49:13 +0530

    Updated the Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,11 @@ type NavbarProps = {
 export default function Navbar({ isAuthenticated, signOut }: NavbarProps) {
     return (
         <nav className="navbar">
-            <Link to="/"><p className="text-2xl font-bold text-gradient">ResumeMate</p></Link>
+            <Link to="/">
+                <p className="text-2xl font-bold text-gradient block lg:hidden">RM</p>
+                <p className="text-2xl font-bold text-gradient hidden lg:block">ResumeMate</p>
+            </Link>
+
             <div className="flex gap-2">
                 <Link to="/upload" className="primary-button w-fit">Upload Resume</Link>
                 {isAuthenticated && <button className="primary-button w-fit" onClick={signOut}><p>Sign Out</p></button>}
